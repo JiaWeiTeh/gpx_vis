a1c4a1f10121777462961183419d6bb82f5db497
JiaWeiTeh <teh_jiawei@yahoo.com>
2024-04-05 16:04:28 +0200

    first commit.

---
+++ example/example.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  3 19:44:24 2024
+
+@author: Jia Wei Teh
+"""
+# from gpx_vis import Track
+
+# path2folder = r'/Users/jwt/Documents/Code/Courseworks/random/komoot/data'
+
+# track = Track(path2folder)
+
+# track.create_map('testhere')
